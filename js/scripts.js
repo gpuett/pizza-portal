465fc2d641d3cb775147704601d752664b908e55
Garnett Puett <gpuett@gmail.com>
2018-04-06 09:47:52 -0700

    Change toppings to checkbox input.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,18 @@ Pizza.prototype.price = function() {
   return parseInt(this.size) + parseInt(this.toppings) + 10;
 }
 
+
+
 // user interface logic
 $(document).ready(function() {
   $("#order").submit(function(event) {
     event.preventDefault();
 
     var inputSize = $("#size").val();
-    var inputToppings = $("#toppings").val();
-    console.log(inputSize);
+    var inputToppings = $("input:checkbox[name=toppings]:checked").each(function() {
+       += parseInt($(this).val());
+    });
+    console.log(inputToppings);
     var newPizza = new Pizza(inputSize, inputToppings);
     console.log(newPizza.price());
     $("#output").text("The price of your order will be $" + newPizza.price() +".");
