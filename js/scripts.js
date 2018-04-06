34af1467635193c64583648ccb7341a7cc3c6a6f
Garnett Puett <gpuett@gmail.com>
2018-04-06 14:34:21 -0700

    delete needless script section.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
+  this.price = price;
 }
 
 Pizza.prototype.price = function() {
@@ -12,39 +13,6 @@ Pizza.prototype.price = function() {
 // user interface logic
 $(document).ready(function() {
 
-  // $("#add-pizza").click(function() {
-  //   $("#pizza").append('<div class="new-pizza">' +
-  //                       '<div class="row">' +
-  //                         '<div class="col col-md">' +
-  //                           '<label for="size">Choose your size:</label>' +
-  //                           '<select class="form-control" id="size">' +
-  //                             '<option value="2">Small</option>' +
-  //                             '<option value="4">Medium</option>' +
-  //                             '<option value="8">Large</option>' +
-  //                             '<option value="10">Family</option>' +
-  //                           '</select>' +
-  //                         '</div>' +
-  //                         '<div class="col col-md">' +
-  //                           '<label for="toppings">Choose your toppings:</label>' +
-  //                           '<div class="form-check">' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Red Onion<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Jalapeno Peppers<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Roasted Garlic<br>' +
-  //                             '<input class="form-check-input"  type="checkbox" name="toppings" value="1">Green Peppers<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Black Olives<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Kalamata Olives<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Mushrooms<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Spinach<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Extra Cheese<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="1">Feta Cheese<br>' +
-  //                             '<label for="dietary">Dietary Options</label><br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="2">Gluten Free<br>' +
-  //                             '<input class="form-check-input" type="checkbox" name="toppings" value="-2">Dairy Free<br>' +
-  //                           '</div>' +
-  //                         '</div>' +
-  //                       '</div>' +
-  //                     '</div>');
-  // });
 
   $("#order").submit(function(event) {
     event.preventDefault();
@@ -55,6 +23,7 @@ $(document).ready(function() {
        inputToppings += parseInt($(this).val());
     });
     var newPizza = new Pizza(inputSize, inputToppings);
+
 
 
     $("#output").text("The price of your order will be $" + newPizza.price() +".");
